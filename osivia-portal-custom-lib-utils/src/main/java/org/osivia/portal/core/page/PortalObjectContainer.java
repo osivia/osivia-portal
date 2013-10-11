5e75b79b70e544d56679926e3c275b31587628fa
Cédric Krommenhoek <ckrommenhoek@osivia.com>
2013-10-11 14:42:21 +0000

    Package d'installation (refs #114).

--- osivia-portal-custom-lib-utils/src/main/java/org/osivia/portal/core/page/PortalObjectContainer.java
+++ osivia-portal-custom-lib-utils/src/main/java/org/osivia/portal/core/page/PortalObjectContainer.java
@@ -1,9 +1,8 @@
 package org.osivia.portal.core.page;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.jboss.portal.core.impl.model.portal.ContextImpl;
-import org.jboss.portal.core.impl.model.portal.ObjectNode;
 import org.jboss.portal.core.impl.model.portal.PersistentPortalObjectContainer;
 import org.jboss.portal.core.model.portal.Context;
 import org.jboss.portal.core.model.portal.PortalObject;
@@ -17,23 +16,23 @@ import org.osivia.portal.core.portalobjects.IDynamicObjectContainer;
 
 
 /**
- * 
- * Ce module surcharge le container par defaut de JBoss Portal 
- * 
+ *
+ * Ce module surcharge le container par defaut de JBoss Portal
+ *
  * @author jsteux
  *
  */
 public class PortalObjectContainer extends PersistentPortalObjectContainer {
 
-	private Log logger = LogFactory.getLog(PortalObjectContainer.class);
-	
+	private final Log logger = LogFactory.getLog(PortalObjectContainer.class);
+
 	private IDynamicObjectContainer dynamicObjectContainer = null;
 	private String dynamicObjectContainerBeanName = null;
 
 
-	
+
 	public String getDynamicObjectContainerBeanName() {
-		return dynamicObjectContainerBeanName;
+		return this.dynamicObjectContainerBeanName;
 	}
 
 	public void setDynamicObjectContainerBeanName(String dynamicObjectContainerBeanName) {
@@ -42,40 +41,46 @@ public class PortalObjectContainer extends PersistentPortalObjectContainer {
 
 	public IDynamicObjectContainer getDynamicObjectContainer() {
 		try	{
-			if( dynamicObjectContainer == null)
-				dynamicObjectContainer = Locator.findMBean(IDynamicObjectContainer.class, getDynamicObjectContainerBeanName());
+			if( this.dynamicObjectContainer == null) {
+                this.dynamicObjectContainer = Locator.findMBean(IDynamicObjectContainer.class, this.getDynamicObjectContainerBeanName());
+            }
 			} catch( Exception e)	{
-				logger.error(e);
+				this.logger.error(e);
 			}
-			return dynamicObjectContainer;
-		
+			return this.dynamicObjectContainer;
+
 	}
 
 	public PortalObject getNonDynamicObject(PortalObjectId id) throws IllegalArgumentException {
-	
+
 		return super.getObject(id);
 
 
 	}
 
 	public PortalObject getObject(PortalObjectId id) throws IllegalArgumentException {
-		
-		PortalObject object = getDynamicObjectContainer().getObject(this, id);
+
+		PortalObject object = this.getDynamicObjectContainer().getObject(this, id);
 
 		return object;
 
 
 	}
-	
-	
-	  public Context getContext(String namespace)
-	   {
-		  
-		  PortalObject object = getDynamicObjectContainer().getObject(this, new PortalObjectId("", PortalObjectPath.ROOT_PATH));
 
-		return (Context) object;
 
-	   }
-	
-	
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Context getContext(String namespace) {
+        String newNamespace = namespace;
+        if (newNamespace == null) {
+            newNamespace = StringUtils.EMPTY;
+        }
+
+        PortalObject object = this.getDynamicObjectContainer().getObject(this, new PortalObjectId(newNamespace, PortalObjectPath.ROOT_PATH));
+
+        return (Context) object;
+    }
+
 }
