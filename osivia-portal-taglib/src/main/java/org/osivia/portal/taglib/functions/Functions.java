83776de7dbaa666736e2f0d2c7ee7c269075c3d7
Cédric Krommenhoek <ckrommenhoek@osivia.com>
2015-09-28 07:47:40 +0000

    Taglib.

---
+++ osivia-portal-taglib/src/main/java/org/osivia/portal/taglib/functions/Functions.java
@@ -0,0 +1,22 @@
+package org.osivia.portal.taglib.functions;
+
+import org.apache.commons.lang.StringEscapeUtils;
+
+/**
+ * Tag functions.
+ *
+ * @author Cédric Krommenhoek
+ */
+public class Functions {
+
+    /**
+     * Escapes the characters in a String using JavaScript String rules.
+     *
+     * @param str String to escape values in, may be null
+     * @return String with escaped values, null if null string input
+     */
+    public static String escapeJavaScript(String str) {
+        return StringEscapeUtils.escapeJavaScript(str);
+    }
+
+}
