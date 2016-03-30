044a123064c34e09e070356bcd42269a76160a73
Cédric Krommenhoek <ckrommenhoek@osivia.com>
2016-03-30 13:20:50 +0000

    Domain contextualization.

--- osivia-portal-custom-lib-utils/src/main/java/org/osivia/portal/core/cms/DomainContextualization.java
+++ osivia-portal-custom-lib-utils/src/main/java/org/osivia/portal/core/cms/DomainContextualization.java
@@ -12,13 +12,31 @@ import org.osivia.portal.api.context.PortalControllerContext;
  */
 public interface DomainContextualization {
 
-
+    /**
+     * Check if domain must be contextualized.
+     * 
+     * @param portalControllerContext portal controller context
+     * @param domainPath domain path
+     * @return true if domain must be contextualized
+     */
     boolean contextualize(PortalControllerContext portalControllerContext, String domainPath);
 
 
+    /**
+     * Get domain site names.
+     * 
+     * @param portalControllerContext portal controller context
+     * @return site names
+     */
     List<String> getSites(PortalControllerContext portalControllerContext);
 
 
+    /**
+     * Get domain default site name, may be null.
+     * 
+     * @param portalControllerContext portal controller context
+     * @return default site name
+     */
     String getDefaultSite(PortalControllerContext portalControllerContext);
 
 }
