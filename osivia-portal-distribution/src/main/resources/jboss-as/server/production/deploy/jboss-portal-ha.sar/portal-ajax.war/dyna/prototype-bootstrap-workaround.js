1a4d0c76157c792a8f14f956e9f40f68cc752c59
Dorian Licois <licois.dorian@gmail.com>
2016-09-26 07:57:58 +0000

    ajout des tab dans la liste des plugin ou prototype est désactivé afin d'éviter les soucis graphiques

--- osivia-portal-distribution/src/main/resources/jboss-as/server/production/deploy/jboss-portal-ha.sar/portal-ajax.war/dyna/prototype-bootstrap-workaround.js
+++ osivia-portal-distribution/src/main/resources/jboss-as/server/production/deploy/jboss-portal-ha.sar/portal-ajax.war/dyna/prototype-bootstrap-workaround.js
@@ -12,7 +12,7 @@ if (Prototype.BrowserFeatures.ElementExtensions) {
                 jQuery(window).on(method + '.bs.' + plugin, handler);
             });
         },
-        pluginsToDisable = ['collapse', 'dropdown', 'modal', 'tooltip', 'popover'];
+        pluginsToDisable = ['collapse', 'dropdown', 'modal', 'tooltip', 'popover', 'tab'];
     disablePrototypeJS('show', pluginsToDisable);
     disablePrototypeJS('hide', pluginsToDisable);
 }
