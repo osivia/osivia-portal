0545ef4fb22d7004e94315393ebfd9b92ba40a72
Jean-Sébastien Steux <jssteux@osivia.com>
2016-10-07 16:15:56 +0000

    refresh sur session expirée, fixes #1288

--- osivia-portal-custom-services/src/main/java/org/osivia/portal/core/page/RefreshPageCommand.java
+++ osivia-portal-custom-services/src/main/java/org/osivia/portal/core/page/RefreshPageCommand.java
@@ -14,20 +14,28 @@
  */
 package org.osivia.portal.core.page;
 
+import javax.xml.XMLConstants;
+import javax.xml.namespace.QName;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.jboss.portal.api.PortalURL;
 import org.jboss.portal.core.controller.ControllerCommand;
 import org.jboss.portal.core.controller.ControllerContext;
 import org.jboss.portal.core.controller.ControllerException;
 import org.jboss.portal.core.controller.ControllerResponse;
 import org.jboss.portal.core.controller.command.info.ActionCommandInfo;
 import org.jboss.portal.core.controller.command.info.CommandInfo;
+import org.jboss.portal.core.controller.command.response.RedirectionResponse;
 import org.jboss.portal.core.model.portal.Page;
 import org.jboss.portal.core.model.portal.PortalObject;
 import org.jboss.portal.core.model.portal.PortalObjectId;
 import org.jboss.portal.core.model.portal.PortalObjectPath;
 import org.jboss.portal.core.model.portal.command.response.UpdatePageResponse;
+import org.jboss.portal.core.model.portal.command.view.ViewPageCommand;
+import org.jboss.portal.core.model.portal.navstate.PageNavigationalState;
+import org.jboss.portal.core.navstate.NavigationalStateContext;
 import org.osivia.portal.api.context.PortalControllerContext;
 import org.osivia.portal.api.internationalization.IInternationalizationService;
 import org.osivia.portal.api.locator.Locator;
@@ -42,6 +50,7 @@ import org.osivia.portal.core.internationalization.InternationalizationUtils;
 import org.osivia.portal.core.notifications.NotificationsUtils;
 import org.osivia.portal.core.page.PageProperties;
 import org.osivia.portal.core.portalobjects.CMSTemplatePage;
+import org.osivia.portal.core.portalobjects.DynamicPersistentPage;
 
 
 public class RefreshPageCommand extends ControllerCommand {
@@ -112,6 +121,31 @@ public class RefreshPageCommand extends ControllerCommand {
 		// Récupération page
 		PortalObjectId poid = PortalObjectId.parse(pageId, PortalObjectPath.SAFEST_FORMAT);
 		PortalObject page = getControllerContext().getController().getPortalObjectContainer().getObject(poid);
+		
+		
+		
+		// En cas de perte de session, les pages statiques CMS ne sont pas correctement rafraichies
+		// (liées à la perte des paramètres de la page)
+		// On reprovoque un init-state
+		
+		if( ( page instanceof DynamicPersistentPage) && (page.getDeclaredProperty("osivia.cms.basePath") != null))    {
+            NavigationalStateContext stateContext = (NavigationalStateContext) getControllerContext().getAttributeResolver(
+                    ControllerCommand.NAVIGATIONAL_STATE_SCOPE);
+            // Current page state
+            PageNavigationalState pageState = stateContext.getPageNavigationalState(page.getId().toString());
+
+            String[] sPath = null;
+            if (pageState != null) {
+                sPath = pageState.getParameter(new QName(XMLConstants.DEFAULT_NS_PREFIX, "osivia.cms.path"));
+            }
+
+            if (sPath == null) {
+                ViewPageCommand pageCmd = new ViewPageCommand(page.getId());
+                PortalURL url = new PortalURLImpl(pageCmd, getControllerContext(), null, null);
+                String redirectUrl = url.toString() + "?init-state=true";
+                return new RedirectionResponse(redirectUrl);
+            }
+		}
 
 		PageProperties.getProperties().setRefreshingPage(true);
 		//getControllerContext().setAttribute(REQUEST_SCOPE, "osivia.refreshPage", "1");
