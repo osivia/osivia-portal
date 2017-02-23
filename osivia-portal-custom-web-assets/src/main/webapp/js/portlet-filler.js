603da7648f913fff90822b84f265d7cb3d2004c7
Cédric Krommenhoek <ckrommenhoek@osivia.com>
2017-02-23 10:36:07 +0000

    Layout fixés.

---
+++ osivia-portal-custom-web-assets/src/main/webapp/js/portlet-filler.js
@@ -0,0 +1,42 @@
+$JQry(function() {
+	var $portletFiller = $JQry("body.fixed-layout .portlet-filler");
+	
+	// Portlet filler parents flexbox class
+	$portletFiller.parentsUntil(".flexbox").addClass("flexbox");
+	
+	// Update scrollbar width
+	updateScrollbarWidth();
+});
+
+
+// Update scrollbar width on window resize
+$JQry(window).resize(function() {
+	updateScrollbarWidth();
+});
+
+
+/**
+ * Update scrollbar width.
+ */
+function updateScrollbarWidth() {
+	var $window = $JQry(window),
+		$portletFiller = $JQry("body.fixed-layout .portlet-filler");
+	
+	$portletFiller.each(function(index, element) {
+		var $element = $JQry(element),
+			$table = $element.closest(".table"),
+			$tableHeader = $table.find(".table-header"),
+			scrollbarWidth;
+	
+		if ($window.width() >= 768) {
+			scrollbarWidth = Math.round($element.innerWidth() - $element.children().outerWidth(true));
+		} else {
+			scrollbarWidth = 0;
+		}
+		
+		// Update table header
+		$tableHeader.find(".row").first().css({
+			"padding-right": scrollbarWidth
+		});
+	});
+}
