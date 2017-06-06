91b594671e8a570323919756350e916615fad697
Cédric Krommenhoek <ckrommenhoek@osivia.com>
2017-06-06 09:09:22 +0000

    Correction des interractions Ajax avec le composant bxSlider (fixes #1476).

--- osivia-portal-custom-web-assets/src/main/webapp/js/bxslider-integration.js
+++ osivia-portal-custom-web-assets/src/main/webapp/js/bxslider-integration.js
@@ -1,15 +1,32 @@
-var $sliderDefault;
-
 $JQry(function() {
-	
-	if (typeof $sliderDefault === "undefined") {
-		$sliderDefault = $JQry(".bxslider.bxslider-default").bxSlider({
-			// Controls
-			autoControls : true,  // If true, "Start" / "Stop" controls will be added
+	$JQry(".bxslider.bxslider-default").each(function(index, element) {
+		var $element = $JQry(element),
+			loaded = $element.data("loaded"),
+			pause = $element.data("pause");
+		
+		if (!loaded) {
+			if (pause === undefined) {
+				pause = 4000;
+			} else {
+				pause *= 1000;
+			}
 			
-			// Auto
-			auto : true  // Slides will automatically transition
-		});
-	}
+			$element.bxSlider({
+				// General
+				mode : "horizontal",  // Type of transition between slides
+				
+				// Controls
+				autoControls : true,  // If true, "Start" / "Stop" controls will be added
+				
+				// Auto
+				auto : true,  // Slides will automatically transition
+				pause : pause,  // The amount of time (in ms) between each auto transition
+				autoHover : true,  // Auto show will pause when mouse hovers over fgtSlider
+				autoDelay : 3000  // Time (in ms) auto show should wait before starting
+			});
+			
+			$element.data("loaded", true);
+		}
+	});
 	
 });
