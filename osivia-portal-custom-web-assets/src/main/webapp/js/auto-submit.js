a1d7407d19c5b550681e6f3efeff9b1cc38fad9c
Cédric Krommenhoek <ckrommenhoek@osivia.com>
2017-10-31 18:03:18 +0000

    Suppression du JavaScript spécifique de la fiche personne (ne peut pas être chargé en Ajax pour le moment).

---
+++ osivia-portal-custom-web-assets/src/main/webapp/js/auto-submit.js
@@ -0,0 +1,18 @@
+$JQry(function() {
+	
+	$JQry("input[data-change-submit]").each(function(index, element) {
+		var $element = $JQry(element);
+		
+		if (!$element.data("loaded")) {
+			$element.change(function(event) {
+				var $target = $JQry(event.target),
+					$submit = $JQry("#" + $target.data("change-submit"));
+				
+				$submit.click();
+			});
+			
+			$element.data("loaded", true);
+		}
+	});
+	
+});
