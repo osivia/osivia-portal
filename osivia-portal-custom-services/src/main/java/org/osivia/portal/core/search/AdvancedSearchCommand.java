d92fd1f40a200f95e1e65b5d6350b2c43c9e1177
Cédric Krommenhoek <ckrommenhoek@osivia.com>
2018-02-01 16:50:36 +0000

    Protection des contenus saisis afin d'éviter l'injection (fixes #1787).

--- osivia-portal-custom-services/src/main/java/org/osivia/portal/core/search/AdvancedSearchCommand.java
+++ osivia-portal-custom-services/src/main/java/org/osivia/portal/core/search/AdvancedSearchCommand.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.StringUtils;
 import org.jboss.portal.core.controller.ControllerCommand;
 import org.jboss.portal.core.controller.ControllerException;
@@ -65,7 +66,7 @@ public class AdvancedSearchCommand extends ControllerCommand {
      * @param advancedSearch advanced search indicator
      */
     public AdvancedSearchCommand(String search, boolean advancedSearch) {
-        this.search = search;
+        this.search = StringEscapeUtils.escapeHtml(search);
         this.advancedSearch = advancedSearch;
     }
 
