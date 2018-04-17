a96ee2670388998ea57f088de9de1810a436e49e
Dorian Licois <licois.dorian@gmail.com>
2018-04-17 08:48:26 +0000

    refs #1840 : correction d'un bug dans le décodage des windowProperty (lorsque l'on stocke un "==" URLencodé : "%3D%3D")

--- osivia-portal-custom-services/src/main/java/org/osivia/portal/core/urls/WindowPropertiesEncoder.java
+++ osivia-portal-custom-services/src/main/java/org/osivia/portal/core/urls/WindowPropertiesEncoder.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2014 OSIVIA (http://www.osivia.com) 
+ * (C) Copyright 2014 OSIVIA (http://www.osivia.com)
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -19,71 +19,79 @@ import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
+
 public class WindowPropertiesEncoder {
 
-	public static String encodeProperties(Map<String, String> props) {
+    public static String encodeProperties(Map<String, String> props) {
 
-		try {
-			String url = "";
+        try {
+            String url = "";
 
-			for (String name : props.keySet()) {
-				if (props.get(name) != null) {
-					if (url.length() > 0)
-						url += "&&";
-					url += encodeValue(name) + "==" + encodeValue(props.get(name));
-				}
-			}
+            for (String name : props.keySet()) {
+                if (props.get(name) != null) {
+                    if (url.length() > 0) {
+                        url += "&&";
+                    }
+                    url += encodeValue(name) + "==" + encodeValue(props.get(name));
+                }
+            }
 
-			return URLEncoder.encode(url, "UTF-8");
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
+            return URLEncoder.encode(url, "UTF-8");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public static Map<String, String> decodeProperties(String urlParams) {
-		try {
+    public static Map<String, String> decodeProperties(String urlParams) {
+        try {
 
-			Map<String, String> params = new HashMap<String, String>();
+            Map<String, String> params = new HashMap<>();
 
-			if (urlParams == null || urlParams.length() == 0)
-				return params;
+            if (urlParams == null || urlParams.length() == 0) {
+                return params;
+            }
 
-			String decodedParam = URLDecoder.decode(urlParams, "UTF-8");
+            String decodedParam = URLDecoder.decode(urlParams, "UTF-8");
 
-			String[] tabParams = decodedParam.split("&&");
+            String[] tabParams = decodedParam.split("&&");
 
-			for (int i = 0; i < tabParams.length; i++) {
-				String[] valParams = tabParams[i].split("==");
-				
-				if (valParams.length != 1 && valParams.length != 2)
-					throw new IllegalArgumentException("Bad parameter format");
-			
-				String value = "";
-				
-				if( valParams.length == 2)
-					value = valParams[1];
+            for (String tabParam : tabParams) {
 
-				params.put(valParams[0], value);
-			}
+                String name = tabParam;
+                String value = StringUtils.EMPTY;
 
-			return params;
+                int separatorPos = tabParam.indexOf("==");
+                // #1840 on conserve tout ce qui suit le premier séparateur, même s'il y en a d'autres car il peut arriver qu'une windowProperty ait été décodée
+                // (URLDecoder) plusieurs fois (notamment lors du parsing de la QueryParameterMap
+                if (separatorPos != StringUtils.INDEX_NOT_FOUND) {
+                    name = tabParam.substring(0, separatorPos);
+                    value = tabParam.substring(separatorPos + 2);
+                }
 
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+                params.put(name, value);
+            }
 
-	}
+            return params;
 
-	private static String encodeValue(String origValue) {
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-		if (origValue.contains("=="))
-			throw new RuntimeException("Bad parameter format");
-		
-		if (origValue.contains("&&"))
-			throw new RuntimeException("Bad parameter format");
+    }
 
-		return origValue;
+    private static String encodeValue(String origValue) {
 
-	}
+        if (origValue.contains("==")) {
+            throw new RuntimeException("Bad parameter format");
+        }
+
+        if (origValue.contains("&&")) {
+            throw new RuntimeException("Bad parameter format");
+        }
+
+        return origValue;
+
+    }
 
 }
