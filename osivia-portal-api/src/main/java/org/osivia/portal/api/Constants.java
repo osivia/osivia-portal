7e8fdd15b2b833295a20d6043356d2f3dfae1984
Jean-Sébastien <Jean-Sbastien@DESKTOP-IKBRQGB>
2018-06-26 20:09:31 +0200

    TDB procédures : il faut requeter les lives

--- osivia-portal-api/src/main/java/org/osivia/portal/api/Constants.java
+++ osivia-portal-api/src/main/java/org/osivia/portal/api/Constants.java
@@ -214,7 +214,8 @@ public final class Constants {
 
     /** Portlet CMS URI request attribute name. */
     public static final String REQUEST_ATTR_URI = WINDOW_PROP_URI;
-
+    /** The version of the content. */
+    public static final String REQUEST_ATTR_VERSION = WINDOW_PROP_VERSION;
 
     // SESSION
 
