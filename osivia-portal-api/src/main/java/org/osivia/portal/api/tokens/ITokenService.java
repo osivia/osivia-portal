f65bf64863ac06a246cb9457b7ba082bcbb3c011
Jean-Sébastien <Jean-Sbastien@DESKTOP-IKBRQGB>
2019-03-27 09:59:12 +0100

    #1964 mise en cluster tokens

--- osivia-portal-api/src/main/java/org/osivia/portal/api/tokens/ITokenService.java
+++ osivia-portal-api/src/main/java/org/osivia/portal/api/tokens/ITokenService.java
@@ -17,21 +17,27 @@ public interface ITokenService {
     static final String MBEAN_NAME = "osivia:service=TokenService";
 
 	/**
+	 * Create a new token associated with attributes
+	 * 
 	 * @param attributes
 	 * @return
 	 */
 	String generateToken(Map<String, String> attributes);
 
 	/**
+	 * Validate the token
+	 * 
 	 * @param tokenKey
 	 * @param renew
-	 * @return
+	 * @return the original attributes
 	 */
 	Map<String, String> validateToken(String tokenKey, boolean renew);
 
 	/**
+	 * Validate (and remove) the token
+	 * 
 	 * @param tokenKey
-	 * @return
+	 * @return the original attributes
 	 */
 	Map<String, String> validateToken(String tokenKey);
     
--- osivia-portal-custom-services/src/main/java/org/osivia/portal/core/tokens/TokenService.java
+++ osivia-portal-custom-services/src/main/java/org/osivia/portal/core/tokens/TokenService.java
@@ -1,14 +1,29 @@
-/**
- * 
+/*
+ * (C) Copyright 2014 OSIVIA (http://www.osivia.com)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
  */
 package org.osivia.portal.core.tokens;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jboss.cache.CacheException;
+import org.jboss.cache.Fqn;
+import org.jboss.cache.TreeCacheMBean;
 import org.osivia.portal.api.tokens.ITokenService;
-import org.osivia.portal.api.tokens.Token;
 
 /**
  * @author loic
@@ -16,34 +31,104 @@ import org.osivia.portal.api.tokens.Token;
  */
 public class TokenService implements ITokenService {
 
-    private Map<String, Token> tokens = new ConcurrentHashMap<>();
-    private long TOKEN_TIMEOUT = 120000L;
+    /** Logger. */
+    private static final Log logger = LogFactory.getLog(TokenService.class);
 
+    /** Cache for tokens */
+    
+    private static final String OSIVIA_TOKENS = "osivia/tokens";
+    private TreeCacheMBean treeCache;
+
+
+    /**
+     * Getter for treeCache.
+     * 
+     * @return the treeCache
+     */
+    public TreeCacheMBean getTreeCache() {
+        return treeCache;
+    }
+
+
+    /**
+     * Setter for treeCache.
+     * 
+     * @param treeCache the treeCache to set
+     */
+    public void setTreeCache(TreeCacheMBean treeCache) {
+        this.treeCache = treeCache;
+    }
+
+    /**
+     *  Timeout for token validity (10 minutes)
+     */
+    private long TOKEN_TIMEOUT = 600000L;
+
+    /* (non-Javadoc)
+     * @see org.osivia.portal.api.tokens.ITokenService#generateToken(java.util.Map)
+     */
+    
     public String generateToken(Map<String, String> attributes) {
+
+        if( logger.isDebugEnabled())
+            logger.debug("generateToken");
         
         String tokenKey = new String(Base64.encodeBase64(("key_" + System.currentTimeMillis()).getBytes()));
-        Token idToken = new Token(attributes);
-        tokens.put(tokenKey, idToken);
-        return tokenKey;
-    }
+        
+        // Hashmap is ready for serialization
+        HashMap<String, String> serMap = new HashMap<>(attributes);
+        Token token = new Token(serMap);
 
-
-    public Map<String, String> validateToken(String tokenKey, boolean renew) {
-        Map<String, String> attributes = null;
-        Token token = tokens.get(tokenKey);
-        if (token != null) {
-            long ts = System.currentTimeMillis();
-            if (ts - token.getCreationTs() < TOKEN_TIMEOUT) {
-                attributes = token.getAttributes();
-                if(! renew)
-                    tokens.remove(tokenKey);
-            }
+        try {
+            getTreeCache().put(getFqn(), tokenKey, token);
+            return tokenKey;
+        } catch (CacheException e) {
+            throw new RuntimeException(e);
         }
-        return attributes;
+
     }
 
+
+    private Fqn getFqn() {
+        Fqn fqn = new Fqn(OSIVIA_TOKENS);
+        return fqn;
+    }
+
+
+    /* (non-Javadoc)
+     * @see org.osivia.portal.api.tokens.ITokenService#validateToken(java.lang.String, boolean)
+     */
+    public Map<String, String> validateToken(String tokenKey, boolean renew) {
+        
+        if( logger.isDebugEnabled())
+            logger.debug("validateToken" + tokenKey);
+        
+        Map<String, String> attributes = null;
+        Token token;
+        try {
+            token = (Token) getTreeCache().get(getFqn(), tokenKey);
+
+            if (token != null) {
+                long ts = System.currentTimeMillis();
+                if (ts - token.getCreationTs() < TOKEN_TIMEOUT) {
+                    attributes = token.getAttributes();
+                    if (!renew) {
+                        getTreeCache().remove(getFqn(), tokenKey);
+                    }
+                }
+            }
+            return attributes;
+        } catch (CacheException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.osivia.portal.api.tokens.ITokenService#validateToken(java.lang.String)
+     */
     public Map<String, String> validateToken(String tokenKey) {
         return validateToken(tokenKey, false);
-
     }
+
+
 }
