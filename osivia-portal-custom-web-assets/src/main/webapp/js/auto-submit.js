b36d2fce5b664b10cf714068cce2cfff2039f547
Cédric Krommenhoek <cedric.krommenhoek@gmail.com>
2019-09-16 15:51:37 +0200

    :sparkles: Auto-submit for <textarea> and <select> HTML components.

--- osivia-portal-custom-web-assets/src/main/webapp/js/auto-submit.js
+++ osivia-portal-custom-web-assets/src/main/webapp/js/auto-submit.js
@@ -1,6 +1,6 @@
 $JQry(function() {
 	
-	$JQry("input[data-change-submit]").each(function(index, element) {
+	$JQry("input[data-change-submit], textarea[data-change-submit], select[data-change-submit]").each(function(index, element) {
 		var $element = $JQry(element);
 
 		if (!$element.data("loaded")) {
