01568d8e5383b8306fa601028bf3ddb19b0880bd
Cédric Krommenhoek <cedric.krommenhoek@gmail.com>
2020-12-10 16:22:46 +0100

    🐛 Add default constructor to profil for XML parsing.

--- osivia-portal-custom-services/src/main/java/org/osivia/portal/core/profils/XMLSerializer.java
+++ osivia-portal-custom-services/src/main/java/org/osivia/portal/core/profils/XMLSerializer.java
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2014 OSIVIA (http://www.osivia.com) 
+ * (C) Copyright 2014 OSIVIA (http://www.osivia.com)
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
@@ -14,87 +14,87 @@
  */
 package org.osivia.portal.core.profils;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.osivia.portal.core.profils.ProfilBean;
-
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class XMLSerializer {
-	
-	protected static final Log logger = LogFactory.getLog(XMLSerializer.class);
-	
-	public static class Profils {
-		
-		List<ProfilBean> implicitProfils ;
 
-		public List<ProfilBean> getImplicitProfils() {
-			return implicitProfils;
-		}
+    protected static final Log logger = LogFactory.getLog(XMLSerializer.class);
 
-		public Profils(List<ProfilBean> profils) {
-			super();
-			this.implicitProfils = profils;
-		}
-	 }
+    public String encodeAll(List<ProfilBean> listToEncode) {
 
-	
-	
-	public String encodeAll(List<ProfilBean> listToEncode) {	
-		
-		
-		XStream xstream = new XStream(new DomDriver());
-		
-		
-		xstream.alias("profil", ProfilBean.class);
-	    xstream.alias("profils", Profils.class);
-	    
-	    xstream.addImplicitCollection(Profils.class, "implicitProfils", "profil", ProfilBean.class);
-	   
-		
 
-		// Convertion du contenu de l'objet article en XML
-		String xmlText = xstream.toXML(new Profils( listToEncode));
+        XStream xstream = new XStream(new DomDriver());
 
-		
-		return xmlText;
 
-	}
+        xstream.alias("profil", ProfilBean.class);
+        xstream.alias("profils", Profils.class);
 
-	public List<ProfilBean> decodeAll(String input) {
-	
-		XStream xstream = new XStream(new DomDriver());
-		
-		
-		xstream.alias("profil", ProfilBean.class);
-	    xstream.alias("profils", Profils.class);
-	    
-	    xstream.addImplicitCollection(Profils.class, "implicitProfils", "profil", ProfilBean.class);
-		
-		if( input == null || input.length() == 0)
-			return new ArrayList<ProfilBean>();		
-		
-		InputStream in = new ByteArrayInputStream(input.getBytes()); 
-		
+        xstream.addImplicitCollection(Profils.class, "implicitProfils", "profil", ProfilBean.class);
 
-		
+
+        // Convertion du contenu de l'objet article en XML
+        String xmlText = xstream.toXML(new Profils(listToEncode));
+
+
+        return xmlText;
+
+    }
+
+    public List<ProfilBean> decodeAll(String input) {
+
+        XStream xstream = new XStream(new DomDriver());
+
+
+        xstream.alias("profil", ProfilBean.class);
+        xstream.alias("profils", Profils.class);
+
+        xstream.addImplicitCollection(Profils.class, "implicitProfils", "profil", ProfilBean.class);
+
+        if (input == null || input.length() == 0)
+            return new ArrayList<ProfilBean>();
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+
+
         // objet article
-		Profils profils = (Profils) xstream.fromXML(in);
-		return profils.getImplicitProfils();
+        Profils profils = (Profils) xstream.fromXML(in);
+        return profils.getImplicitProfils();
 
 
-	}
-	
+    }
+
+    public static class Profils {
+
+        List<ProfilBean> implicitProfils;
+
+        /**
+         * Default constructor.
+         */
+        public Profils() {
+            super();
+        }
+
+        public Profils(List<ProfilBean> profils) {
+            super();
+            this.implicitProfils = profils;
+        }
+
+        public List<ProfilBean> getImplicitProfils() {
+            return implicitProfils;
+        }
+
+        public void setImplicitProfils(List<ProfilBean> implicitProfils) {
+            this.implicitProfils = implicitProfils;
+        }
+    }
+
 }
