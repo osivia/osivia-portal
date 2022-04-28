5773e54f5cd3102bd10d771129982011470e3f3c
Loïc Billon <loic.billon@gmail.com>
2022-04-28 11:37:41 +0200

    Service restricted on logged users

--- osivia-portal-api/src/main/java/org/osivia/portal/api/taskbar/TaskbarItemRestriction.java
+++ osivia-portal-api/src/main/java/org/osivia/portal/api/taskbar/TaskbarItemRestriction.java
@@ -7,6 +7,8 @@ package org.osivia.portal.api.taskbar;
  */
 public enum TaskbarItemRestriction {
 
+	/** Logged user restriction */
+	LOGGED_USER,
     /** Edition restriction. */
     EDITION,
     /** Management restriction. */
